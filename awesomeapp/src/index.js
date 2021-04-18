b707778d7162f99ffc003c952f502f45fc9ff6b1
aakash4069 <aakash4069@gmail.com>
2021-04-18 19:40:43 +0530

    text and image tranfer in jsx form

--- awesomeapp/src/index.js
+++ awesomeapp/src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+let img1="https://picsum.photos/200/300"
+let img2="https://picsum.photos/seed/picsum/200/300"
+let img3="https://picsum.photos/200/300?grayscale"
+let img4="https://picsum.photos/200/300/?blur"
+let link="https://www.github.com";
+let name='Aakash Kushwaha';
+
+const heading={
+  color: "#fa9191",
+  textTransform:"capitalize",
+  textAlign: "center",
+  fontWeight:"bold",
+  textShadow :" 0px 2px 4px #ffe9c5",
+  margin:"70px 0",
+  fontFamily:'"Josfin Sans",sans-serif',
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <>
+    <h1 style={heading} > My name is {name}</h1>
+
+    <div className="img_div">
+      <img src={img1} alt="ramdomImage"/>
+      <img src={img2} alt="ramdomImage"/>
+      <img src={img4} alt="randiomImage"/>
+      <a href={link}>
+
+        <img src={img3} alt="github"/>
+
+      </a>
+    </div>
+  </>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
